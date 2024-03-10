1c16366ccab58f31352571077667cead9e30408a
KittiphongChampa <kittiphong.c@kkumail.com>
2024-03-11 01:58:42 +0700

    'edit'

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -5,6 +5,7 @@ import io from "socket.io-client";
 // import Logo from "../assets/logo.svg";
 import { Link, useParams } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import { Flex } from 'antd';
 
 import { host } from "../utils/api";
 
@@ -45,7 +46,6 @@ export default function Contacts({ contacts, contacts_order, changeChat, Toggled
   };
   return (
     <>
-      
       {userdata.urs_name && userdata.urs_profile_img && (
         <>
 
@@ -62,41 +62,28 @@ export default function Contacts({ contacts, contacts_order, changeChat, Toggled
                       onClick={() => changeCurrentChat(String(contact.id) + String(contact.od_id), contact)}
                     >
                       <img src={contact.urs_profile_img}></img>
+
                       <div>
-                        {!Toggled && (
-                          <>
-                            <p className="order h6">{contact.urs_name}</p>
-                            <p>
-
-                              {contact.message_text?.split("images")[0] === `${host}/` ? (
-                                <p className="message">ได้ส่งรูปภาพ</p>
-                              ) : (
-                                  <p className="message"
-                                    style={{
-                                      display: "flex",
-                                    }}>
-                                    <span className="oneline-textoverflow" style={{ flex: 1}}>{contact.latest_message_text}</span>
-                                    <span style={{ width: "fit-content" }}> {new Date(contact.last_message_time).toLocaleString("th-TH", { timeZone: "Asia/Bangkok", hour: "2-digit", minute: "2-digit", })} น.</span>
-                                  </p>
-                              )}
-                            </p>
-                            {/* <p className="message">{contact.message_text}</p> */}
-                          </>
-                        )}
-
-                        {Toggled && (
-                          <>
-                            <p className="order filterd">xxxx</p>
-                            <p className="message filterd">bust-up full color..</p>
-                            <p className="time filterd">
-                              {" "}
-                              <span className="q">คิวที่1</span>
-                              <span className="stat">รอชำระเงิน</span>
-                            </p>
-                          </>
-                        )}
+                        <p className="order h6">{contact.urs_name}</p>
+                        <Flex>
+                          {contact.message_text?.split("images")[0] === `${host}/` ? (
+                            <p className="message">ได้ส่งรูปภาพ</p>
+                          ) : (
+                              <p className="message"
+                                style={{
+                                  display: "flex",
+                                }}>
+                                <span className="oneline-textoverflow" style={{ flex: 1 }}>{contact.message_text}</span>
+                                <span style={{ width: "fit-content" }}> {new Date(contact.last_message_time).toLocaleString("th-TH", { timeZone: "Asia/Bangkok", hour: "2-digit", minute: "2-digit", })} น.</span>
+                              </p>
+                          )}
+                        </Flex>
+                          {/* <p className="message">{contact.message_text}</p> */}
+                        
                       </div>
+
                     </div>
+
                     <div className="qq">
                       {/* {console.log(contact.id + contact.od_id + "=" + currentSelected)} */}
                       <div className={String(contact.id) + String(contact.od_id) == currentSelected ? "arrow" : ""} />
@@ -119,50 +106,46 @@ export default function Contacts({ contacts, contacts_order, changeChat, Toggled
                       onClick={() => changeCurrentChat(String(contact_order.id) + String(contact_order.od_id), contact_order)}
                     >
                       <img src={contact_order.urs_profile_img}></img>
-                      <div>
-                        {!Toggled && (
-                          <>
-                            <p className="order h6">{contact_order.urs_name}</p>
-                            <p>
+                    <div>
+                        
+                    <p className="order h6">{contact_order.urs_name}</p>
+                    <Flex>
 
-                              {contact_order.message_text?.split("images")[0] === `${host}/` ? (
-                                <p className="message">ได้ส่งรูปภาพ</p>
-                              ) : (
-                                  <p className="message"
-                                    style={{
-                                      display: "flex",
-                                    }}>
-                                    <span className="oneline-textoverflow" style={{ flex: 1}}>{contact_order.latest_message_text}</span>
-                                    <span style={{ width: "fit-content" }}> {new Date(contact_order.last_message_time).toLocaleString("th-TH", { timeZone: "Asia/Bangkok", hour: "2-digit", minute: "2-digit", })} น.</span>
-                                  </p>
-                              )}
-                            </p>
-                            {/* <p className="message">{contact_order.message_text}</p> */}
-                            <p className="time">
-                              {/* <span className="stat">{new Date(contact_order.last_message_time).toLocaleString("th-TH")}</span> */}
-                              {contact_order.od_id != 0 && <> <span className="stat">
-                                {contact_order?.current_step_name?.includes("แนบสลิป") || contact_order?.current_step_name?.includes("ภาพ") ?
-                                  contact_order.artist_id == userdata.id && 'รอ' //ถ้ามีคำว่าสลิปและเราเป็นนักวาด ให้ใส่คำว่ารอ แต่ถ้าไม่มีคำว่าสลิป และเราไม่ใช่นักวาด ให้ใส่คำว่ารอ
-                                  : contact_order.artist_id !== userdata.id && 'รอ'}{contact_order?.current_step_name?.includes("ภาพ") && 'อนุมัติ'}{contact_order.current_step_name}</span>
-                                {/* <span className="stat">{contact_order.od_id}</span> */}
-                                </>
-                              }
-                            </p>
-                          </>
-                        )}
+                      {contact_order.message_text?.split("images")[0] === `${host}/` ? (
+                        <p className="message">ได้ส่งรูปภาพ</p>
+                      ) : (
+                          <p className="message"
+                            style={{
+                              display: "flex",
+                            }}>
+                            <span className="oneline-textoverflow" style={{ flex: 1 }}>{contact_order.message_text}</span>
+                            <span style={{ width: "fit-content" }}> {new Date(contact_order.last_message_time).toLocaleString("th-TH", { timeZone: "Asia/Bangkok", hour: "2-digit", minute: "2-digit", })} น.</span>
+                          </p>
+                      )}
+                    </Flex>
+                    <p className="time">
+                      {contact_order.finished_at == null && contact_order.od_cancel_by == null && contact_order.current_step_name != null &&
+                        <>
+                          <span className="q-number">
+                            คิว {contact_order.od_q_number}
+                          </span>
+                          <span className="stat">
+                            รอ{contact_order?.current_step_name?.includes("ภาพ") && 'อนุมัติ'}{contact_order.current_step_name}
+                          </span>
+                        </>}
+                      {contact_order.finished_at != null &&
+                        <span className="finish">
+                          เสร็จสิ้นแล้ว
+                        </span>
+                      }
+                      {contact_order.od_cancel_by != null &&
+                        <span className="cancel">
+                          ยกเลิกแล้ว
+                        </span>
+                      }
 
-                        {Toggled && (
-                          <>
-                            <p className="order filterd">xxxx</p>
-                            <p className="message filterd">bust-up full color..</p>
-                            <p className="time filterd">
-                              {" "}
-                              <span className="q">คิวที่1</span>
-                              <span className="stat">รอชำระเงิน</span>
-                            </p>
-                          </>
-                        )}
-                      </div>
+                    </p>
+                  </div>
                     </div>
                     <div className="qq">
                       {/* {console.log(contact_order.id + contact_order.od_id + "=" + currentSelected)} */}
