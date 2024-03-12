aa7feef38d2104530c2a38fd15f38c736e234d00
KittiphongChampa <kittiphong.c@kkumail.com>
2024-03-12 19:26:48 +0700

    'deploy'

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,4 @@
 // export const host = "http://localhost:3333";
+// export const phost = "http://127.0.0.1:5000";
 export const host = "https://www.serviceforart.site";
 export const phost = "https://www.serviceforart.site/python"
